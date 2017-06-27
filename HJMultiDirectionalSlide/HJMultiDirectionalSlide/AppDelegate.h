44a073c5ffe926e06304649d49b14c8bf2becf08
fujijing <jing@huangjingjingdeiMac.local>
2017-06-27 15:10:18 +0800

    build a new project

---
+++ HJMultiDirectionalSlide/HJMultiDirectionalSlide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJMultiDirectionalSlide
+//
+//  Created by 黄静静 on 2017/6/27.
+//  Copyright © 2017年 HJing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HJMultiDirectionalSlide/HJMultiDirectionalSlide/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HJMultiDirectionalSlide
+//
+//  Created by 黄静静 on 2017/6/27.
+//  Copyright © 2017年 HJing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
